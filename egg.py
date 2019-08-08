ce6ce962f62bff3529e5bd376c5c27a635fffa48
Alex Glebicka <opipek@gmail.com>
2019-08-08 21:32:20 +0100

    end of chapter 4 projects

---
+++ egg.py
@@ -0,0 +1,27 @@
+def rabbit_list():
+    rabbits = ['this', 'is', 'a','list']
+
+    for rabbits in rabbits:
+        print(rabbits)
+
+rabbit_list()
+
+def spelling():
+    word = "doggo"
+    for letter in word:
+        print(letter)
+spelling()
+
+def cheese_shop():
+    cheese = ['chedar']
+    for cheese in cheese:
+        print('Thats alot of',cheese)
+        break
+    else:
+        print('There isnt any cheese here')
+cheese_shop()
+
+def chicken_eggs():
+    for x in range(0,99):
+        print(x)
+chicken_eggs()
---
+++ login.py
@@ -0,0 +1,47 @@
+def logging_in():
+    password = "cat"
+    username ="Melanie"
+    email = "Melanie@email.com"
+    code="123"
+    new_pass=""
+    updated_pass = new_pass
+
+
+    print('Welcome to Chicken simulator')
+    print("////////////////////")
+
+    password = input("////Password:")
+    username = input("////Username:")
+    if password == "cat" and username == "Melanie":
+        print("////Correct credentials")
+        print("////////////////////")
+    else:
+        print("////////////////////")
+        print("Username or password is incorrect")
+        email = input("To reset your password enter your email:")
+        if email == "Melanie@email.com":
+            print("An email has been sent to your email with a code")
+            code = input("Enter the code:")
+            if code == "123":
+                new_pass = input("Enter a new password:")
+                print("Your password has been changed to",new_pass)
+                print("To continue login with your new password")
+                updated_pass =input("password:")
+                if updated_pass == new_pass:
+                    print("Correct password")
+                    chicken()
+                else:
+                    print('Incorrect password')
+                
+                 
+        else:
+            print("Incorrect email of the account")
+
+        
+            
+            
+logging_in()
+
+
+    
+        
