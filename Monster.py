8d7617fa383a01e573fdbd1e9139e72d1099c2ff
Alex Glebicka <opipek@gmail.com>
2019-08-09 13:27:54 +0100

    Character health depletion added
    The monster now takes away char_health which is now visable.

---
+++ Monster.py
@@ -0,0 +1,72 @@
+#A monster battle , turn based , dmg randomly generated , attacks from a dictionary.
+#User choses , shield , evade and attack.
+#shiled raises defence , meaning dmg will take number of defence into consideration.
+#evade random chance in evasion.
+#attack random number of damage.
+
+import time
+import random
+
+monster_health = 800
+
+char_health = 500
+
+defence = ''
+
+
+
+def battle():
+
+
+    print( '------')
+    print( 'o vv o')
+    print('I    I')
+    print( 'v----v')
+    print('///////')
+    print(monster_health,"/800")
+    time.sleep(1)
+
+battle()
+
+def monster_attk():
+    global char_health
+    global monster_health
+    monster = random.randint(0,100)
+    print('The monster hits you for',monster,'points')
+    char_health -= monster
+
+
+
+def char_attk():
+    global char_health
+    char = random.randint(0,200)
+    print('You hit the monster for',char,'points')
+    global monster_health
+    monster_health -= char
+    ##print(monster_health)
+
+
+def evade():
+    evad = random.randint(1,2)
+    if evad == 1:
+        print('You evaded')
+    elif evad == 2:
+        print('You didnt evade')
+        monster_attk()
+        
+def game():
+    
+    choose = input('1:attack 2:evade:')
+    print("your health:",char_health,"/500")
+    global monster_health
+    while monster_health > 0:
+            if choose == "1" or choose =="attack":
+                char_attk()
+                monster_attk()
+                battle()
+                game()
+            elif choose =="2" or choose =="evade":
+                evade()
+                game()
+                
+game()
--- try.py
+++
@@ -1,4 +0,0 @@
-file = open('useranpass.txt','r')
-print(file.read())
-file.close()
-        
